8ec0264caa832035756704c3341225fadd71cad0
Никита Королев <nickkorol1994@gmail.com>
2020-04-09 12:43:51 +0300

    Initial Commit

---
+++ UIKitPDP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKitPDP
+//
+//  Created by Nikita Korolev on 09.04.2020.
+//  Copyright © 2020 Никита Королев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
